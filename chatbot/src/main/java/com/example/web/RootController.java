d9b350871b4f372964ff0573251220c493af5d5a
sayuri.tsutsui <sayuri.tsutsui@rakuten.com>
2018-03-12 23:19:31 +0900

    修正

--- chatbot/src/main/java/com/example/web/RootController.java
+++ chatbot/src/main/java/com/example/web/RootController.java
@@ -8,7 +8,7 @@ public class RootController {
 
     @GetMapping("/") // (2)@GetMapping
     public String root() {
-    	return "redirect:index";
+    	return "redirect:index"; // 【TODO】リダイレクト先を"chat/index"に変更する
     }
     
     @GetMapping("/index") // (2)@GetMapping
